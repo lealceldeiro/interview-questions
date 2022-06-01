9cc57b2385c3de326398c4cd062bf0ae8d94d72f
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-06-01 07:56:11 +0300

    Clean code

--- src/test/java/com/lealceldeiro/commonword/CommonWordControllerTest.java
+++ src/test/java/com/lealceldeiro/commonword/CommonWordControllerTest.java
@@ -1,6 +1,5 @@
 package com.lealceldeiro.commonword;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
