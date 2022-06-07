bfaa2af2aef1dd8525254bb7838278ac3a552916
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2022-06-07 23:01:37 +0300

    Refactor MStack to improve time complexity when adding an element - From using LinkedList to store the ordered values which used O(n) to ArrayList, by using binary search, now using O(log n)

--- src/main/java/com/lealceldeiro/mcollection/MStack.java
+++ src/main/java/com/lealceldeiro/mcollection/MStack.java
@@ -1,7 +1,6 @@
 package com.lealceldeiro.mcollection;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,42 +8,47 @@ import java.util.List;
  */
 public class MStack {
     private List<Integer> values;
-    private LinkedList<Integer> minToMax;
+    private List<Integer> orderedValues;
 
     public MStack(int capacity) {
         values = new ArrayList<>(capacity);
-        minToMax = new LinkedList<>();
+        orderedValues = new ArrayList<>(capacity);
     }
 
     public void push(Integer e) {
-        if (values.isEmpty()) {
-            minToMax.add(e);
-        } else {
-            int i = 0;
-            for (Integer max : minToMax) {
-                if (max > e) {
-                    break;
-                }
-                i++;
-            }
-            minToMax.add(i, e);
-        }
+        var maxPosition = elementPositionInOrderedValues(e, 0, orderedValues.size() - 1);
+        orderedValues.add(maxPosition, e);
         values.add(e);
+    }
+
+    private int elementPositionInOrderedValues(Integer e, int start, int end) {
+        if (orderedValues.isEmpty()) {
+            return 0;
+        }
+        if (start >= end) {
+            return orderedValues.get(start) > e ? start : start + 1;
+        }
+
+        int middle = start + (end - start) / 2;
+        return orderedValues.get(middle) > e
+               ? elementPositionInOrderedValues(e, start, middle - 1)
+               : elementPositionInOrderedValues(e, middle + 1, end);
     }
 
     public Integer peek() {
         return values.isEmpty() ? null : values.get(values.size() - 1);
     }
+
     public Integer pop() {
         if (values.isEmpty()) {
             return null;
         }
         Integer removed = values.remove(values.size() - 1);
-        minToMax.remove(removed);
+        orderedValues.remove(removed);
         return removed;
     }
 
     public Integer getMax() {
-        return values.isEmpty() ? null : minToMax.getLast();
+        return orderedValues.isEmpty() ? null : orderedValues.get(orderedValues.size() - 1);
     }
 }
